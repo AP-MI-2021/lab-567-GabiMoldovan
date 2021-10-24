b343d2094eed4a53d56f9d64202944a7586bc198
Moldovan Gabi <moldovan.gabi75@gmail.com>
2021-10-24 18:04:38 +0300

    rezolvare fara conentarii si ultima cerinta

--- UI/UserInterface.py
+++ UI/UserInterface.py
@@ -1,4 +1,5 @@
-from functions.Logic import showBooks, UIadaugaCarte, deleteBookbyId, changeBookData
+from functions.Logic import showBooks, UIadaugaCarte, deleteBookbyId, changeBookData, applyDiscounts, modifyTypeByTitle, \
+    printMinPriceforAllTypes, sortByPrice, printDistinctTitlesbyType
 
 
 def printMenu():
@@ -10,8 +11,9 @@ def printMenu():
     print("4. Aplica o reducere de tip silver/gold")
     print("5. Modifica genul pentru un titlu dat")
     print("6. Afiseaza pretul minim pentru fiecare gen")
-    print("7. Afiseaza vanzarile crescator dupa pret")
+    print("7. Ordoneaza vanzarile crescator dupa pret")
     print("8. Afisarea numarului de titluri distincte din fiecare gen")
+    print("9. Undo")
     print("x. Termina programul")
 
 def runMenu(lista):
@@ -22,17 +24,40 @@ def runMenu(lista):
             print("Cartile din librarie sunt:")
             showBooks(lista)
 
-        if option == "1":
+        elif option == "1":
             lista = lista + [UIadaugaCarte(lista)]
             print("Cartea a fost adaugata cu succes!")
 
-        if option == "2":
+        elif option == "2":
             lista = deleteBookbyId(lista)
             print("Cartea a fost stearsa cu succes!")
 
-        if option == "3":
+        elif option == "3":
             lista = changeBookData(lista)
+            print("Datele cartii au fost schimbate cu succes!")
 
-        if option == "x":
+        elif option == "4":
+            lista = applyDiscounts(lista)
+            print("Reducerea a fost aplicata cu succes!")
+
+        elif option == "5":
+            lista = modifyTypeByTitle(lista)
+            print("Modificarea genului cartii a fost aplicata cu succes!")
+
+        elif option == "6":
+            printMinPriceforAllTypes(lista)
+
+        elif option == "7":
+            lista = sortByPrice(lista)
+            print("Ordonarea a avut loc cu succes!")
+
+        elif option == "8":
+            print("Numarul de titluri distincte din fiecare gen sunt:")
+            printDistinctTitlesbyType(lista)
+
+        elif option == "x":
             break
+
+        else:
+            print("Ati ales o optiune gresita!")
         print()
--- functions/Logic.py
+++ functions/Logic.py
@@ -45,3 +45,124 @@ def changeBookData(lista):
         else:
             ListaNoua.append(book)
     return ListaNoua
+
+def applyDiscounts(lista):
+    discountType = input("Dati tipul de reducere(silver/gold/ambele): ")
+    while discountType != "silver" and discountType != "gold" and discountType != "ambele":
+        discountType = input("Dati un tip valid de reducere(silver/gold/ambele): ")
+    ListaNoua = []
+    for book in lista:
+        if getDiscountType(book) == "silver" and ( discountType == "silver" or discountType == "ambele" ):
+            reducere = float(getPrice(book) / 20)
+            reducere = float(getPrice(book) - reducere)
+            newBook = createBook(getId(book), getTitle(book), getBookType(book), reducere, "silver")
+            ListaNoua.append(newBook)
+        elif getDiscountType(book) == "gold" and ( discountType == "gold" or discountType == "ambele" ):
+            reducere = float(getPrice(book) / 10)
+            reducere = float(getPrice(book) - reducere)
+            newBook = createBook(getId(book), getTitle(book), getBookType(book), reducere, "gold")
+            ListaNoua.append(newBook)
+        else:
+            ListaNoua.append(book)
+    return ListaNoua
+
+def checkIfTitleExists(lista, title):
+    for book in lista:
+        if getTitle(book) == title:
+            return True
+    return False
+
+def modifyTypeByTitle(lista):
+    ListaNoua = []
+    title = input("Dati titlul: ")
+    while checkIfTitleExists(lista, title) == False:
+        title = input("Dati un titlu care se afla in librarie: ")
+    newType = input("Dati genul: ")
+    for book in lista:
+        if getTitle(book) == title:
+            newBook = createBook(getId(book), getTitle(book), newType, getPrice(book), getDiscountType(book))
+            ListaNoua.append(newBook)
+        else:
+            ListaNoua.append(book)
+    return ListaNoua
+
+def isInTypeList(ListaGenuri, gen):
+    for genulCartii in ListaGenuri:
+        if genulCartii == gen:
+            return True
+    return False
+
+def getAllTypes(lista):
+    ListaGenuri = []
+    for book in lista:
+        if isInTypeList(ListaGenuri, getBookType(book)) == False:
+            ListaGenuri.append(getBookType(book))
+    return ListaGenuri
+
+def getMinPriceofType(lista, gen):
+    price = None
+    for book in lista:
+        if getBookType(book) == gen:
+            if price == None:
+                price = getPrice(book)
+            elif getPrice(book) < price:
+                price = getPrice(book)
+    return price
+
+def printMinPricebyType(lista, gen):
+    price = getMinPriceofType(lista, gen)
+    for book in lista:
+        if getBookType(book) == gen and getPrice(book) == price:
+            print("\"" + getTitle(book) + "\"", end = ", ")
+    print("cu pretul " + str(price))
+
+
+def printMinPriceforAllTypes(lista):
+    listaGenuri = []
+    listaGenuri = getAllTypes(lista)
+    for genulCartii in listaGenuri:
+        print("Cartea/Cartile cu pretul minim care au genul " + genulCartii + " este/sunt:", end = " ")
+        printMinPricebyType(lista, genulCartii)
+
+def isInPriceList(lista, pret):
+    for price in lista:
+        if price == pret:
+            return True
+    return False
+
+def getAllPrices(lista):
+    prices = []
+    for book in lista:
+        if isInPriceList(prices, getPrice(book)) == False:
+            prices.append(getPrice(book))
+    return prices
+
+def sortByPrice(lista):
+    priceList = []
+    priceList = getAllPrices(lista)
+    priceList.sort()
+    ListaNoua = []
+    for pret in priceList:
+        for book in lista:
+            if getPrice(book) == pret:
+                ListaNoua.append(book)
+    return ListaNoua
+
+def notCounted(titluriGen, titluCarte):
+    for title in titluriGen:
+        if title == titluCarte:
+            return True
+    return False
+
+
+def printDistinctTitlesbyType(lista):
+    listaGenuri = []
+    listaGenuri = getAllTypes(lista)
+    for gen in listaGenuri:
+        titluriGen = []
+        numar = int(0)
+        for book in lista:
+            if getBookType(book) == gen and notCounted(titluriGen, getTitle(book)) == False:
+                numar = numar + int(1)
+                titluriGen.append(getTitle(book))
+        print("Numarul de titluri distincte din genul " + gen + " este: " + str(numar))
